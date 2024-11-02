02b3c6267d0dfce0299af6ae3518b0d88d8307f3
Crownedtechie <nenchi00@gmail.com>
2024-11-02 21:51:03 +0100

    --style: implement the search bar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import arrowIcon from "./assets/icon-arrow.svg"
 
 function App() {
 
   return (
     <>
-      <header>Hi</header>
+      <header>
+        <h1>IP Address Tracker</h1>
+
+        <fieldset>
+          <input type="text" name="" id="" />
+
+          <div>
+            <img src={arrowIcon} alt="arrow icon" />
+          </div>
+        </fieldset>
+      </header>
+
+      <main>
+        
+      </main>
     </>
   )
 }
